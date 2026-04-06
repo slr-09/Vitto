23d050d07b3f45cd8e888af8b5a8bc5e1fbbffc2
slr-09 <ge12090@naver.com>
2026-04-06 23:23:20 +0900

    [#6] feat: VittoWidget extension 초기 설정 및 App Groups 추가

---
+++ VittoWidget/VittoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  VittoWidgetBundle.swift
+//  VittoWidget
+//
+//  Created by 가은 on 4/6/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct VittoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        VittoWidget()
+    }
+}
